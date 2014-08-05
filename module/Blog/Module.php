bb876458874f78134c0458be7b0f935dc5a2731c
Guillermo Devi <g@gbili.com>
2014-08-05 14:38:34 +0200

    force heroku update

--- module/Blog/Module.php
+++ module/Blog/Module.php
@@ -5,6 +5,7 @@ class Module
 {
     public function getAutoloaderConfig()
     {
+        '';
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
                 __DIR__ . '/autoload_classmap.php',
